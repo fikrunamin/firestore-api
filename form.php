dad5d6e69aa0333a19295eea6f295e2d94176834
Fikrun Amin <fikrun65@gmail.com>
2021-04-24 16:01:13 +0800

    Edit index

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+	<form action="index.php" method="post">
+		<input type="text" name="id_masjid">
+		<input type="text" name="status">
+		<input type="submit">
+	</form>
+</body>
+</html>
